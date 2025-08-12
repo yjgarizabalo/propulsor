5fa8114a26d519472ab997e8c92572c2473aee65
yairg <yjgarizabalo@gmail.com>
2025-08-11 22:08:43 -0500

    [propulsor] add tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,astro,html}",
+  ],
+  theme: {
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
